7e675ac6cf74962959b9bb1b753975d854dda1af
Momin Khan <foolmoron@gmail.com>
2018-12-02 15:58:30 -0500

    Initial setup with post processing stack + unity file dialog

---
+++ Assets/Scripts/Extension/GameObjectExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using UnityEngine;
+using UnityEngine.Networking;
+
+public static class GameObjectExtensions {
+
+    public static T GetComponentInSelfOrChildren<T>(this GameObject gameObject) where T : Component {
+        var t = gameObject.GetComponent<T>();
+        if (!t) {
+            t = gameObject.GetComponentInChildren<T>();
+        }
+        return t;
+    }
+
+    public static T GetComponentInSelfOrChildren<T>(this Component component) where T : Component {
+        var t = component.GetComponent<T>();
+        if (!t) {
+            t = component.GetComponentInChildren<T>();
+        }
+        return t;
+    }
+
+    public static bool isNetworked(this GameObject go) {
+        return go.GetComponent<NetworkIdentity>() != null;
+    }
+}
